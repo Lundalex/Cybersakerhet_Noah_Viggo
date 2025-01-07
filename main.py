67fdfe4ec2de02879e7ab259de9edd23ce0ec9b8
abbvighai <viggo.haimanas.bygden@abbgymnasiet.se>
2025-01-07 10:54:34 +0100

    Coded the main application to generate passcodes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# passcode = input("ENTER THE PASSCODE > ")
-# if passcode == "mypass":
-#     print("Flag: CTF220S{akskakdls}")
+import random
+import string
+import threading
 
 def alphabet_encryption(message:str):
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -10,19 +10,40 @@ def alphabet_encryption(message:str):
         encryption += a.replace(character.upper(),"")
     return encryption
 
-myencryption = alphabet_encryption("hello")
+def generate_passcode(length=30): # Generate 30 random uppercase letters
+    passcode = ""
+    for _ in range(length):
+        passcode += random.choice(string.ascii_uppercase)
+    return passcode
 
-for i in range(2):
-    myencryption = alphabet_encryption(myencryption)
+def update_the_passcode():
+    global passcode
+    passcode = generate_passcode()
+    encrypted_message = alphabet_encryption(passcode)
 
-print(myencryption)
+    for _ in range(2):
+        encrypted_message = alphabet_encryption(encrypted_message)
+    with open("message.txt", "w") as encrypted:
+        encrypted.write(encrypted_message)
 
-def find_missing_letter(s):
-    full_alphabet = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    input_set = set(s)
-    missing_letter = full_alphabet - input_set
-    return missing_letter.pop() if missing_letter else None
+    threading.Timer(180, update_the_passcode).start() # update every 3 minutes
 
-def alphabet_decrypt(encrypted_message:str):
-    len()
-    
+passcode_update = threading.Thread(target=update_the_passcode)
+passcode_update.daemon = True  # Quit with main thread
+passcode_update.start()
+
+def main():
+    guess = None
+    while guess != passcode:
+        print("ALPHA83T D3CRYPT10N")
+
+        guess = input("ENTER THE PASSCODE > ")
+
+        if guess == passcode:
+            print("That's correct!")
+            print("Flag: CTF220S{akskakdls}")
+            quit()
+        else:
+            print("INCORRECT!")
+
+main()
